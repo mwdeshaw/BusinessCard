9067642958cd73ddd8ca90a21db078a30580dd3e
mwdeshaw <mwdeshaw@gmail.com>
2019-07-29 17:39:57 -0700

    Initial commit for project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader'],
+        },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }]
+        }]
+    },
+  devtool: 'inline-source-map'
+};
+
