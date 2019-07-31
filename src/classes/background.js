69bc5bdfcf2ff116d2e2ea54cac057bffe516454
mwdeshaw <mwdeshaw@gmail.com>
2019-07-31 10:41:34 -0700

    Clean up files

--- src/classes/background.js
+++
@@ -1,6 +0,0 @@
-class Background {
-    constructor() {
-    }
-}
-
-export default Background;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import Info from './classes/info';
-import Card from './classes/card';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.getElementById("card");
-    const ctx = canvas.getContext("2d");
-
-    const info = new Info(ctx, canvas);
-    new Card(info).start();
 });
