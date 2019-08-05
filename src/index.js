f40b0d6d227949cd91e8094bb0be5242b7f40a6c
mwdeshaw <mwdeshaw@gmail.com>
2019-08-05 12:28:17 -0700

    Fix toggling front and back

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+    const flip = document.getElementById("skills");
+    const front = document.querySelector(".card-inline");
+    const back = document.querySelector(".back");
+    const skillsNew = document.getElementById("skills-new");
+
+    flip.onclick = () => {
+        back.classList.remove("flip");
+        front.classList.add("flip");
+    }
+
+    skillsNew.onclick = () => {
+        front.classList.remove("flip");
+        back.classList.add("flip");
+    }
 });
