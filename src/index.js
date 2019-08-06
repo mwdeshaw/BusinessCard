882bf5f895e0fb0f1b6f5d6d88be74c6336a499b
mwdeshaw <mwdeshaw@gmail.com>
2019-08-05 21:10:01 -0700

    Comment out back flip stuff under development

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const flip = document.getElementById("skills");
-    const front = document.querySelector(".card-outline");
-    const back = document.querySelector(".back");
-    const skillsNew = document.getElementById("skills-new");
+    // const flip = document.getElementById("skills");
+    // const front = document.querySelector(".card-outline");
+    // const back = document.querySelector(".back");
+    // const skillsNew = document.getElementById("skills-new");
 
-    flip.onclick = () => {
-        back.classList.remove("flip");
-        front.classList.add("flip");
-        const vid1 = document.querySelector(".break-mov");
-        vid1.onmouseover = () => {
-            vid1.play();
-        }
-        vid1.onmouseout = () => {
-            vid1.pause();
-        }
-    }
+    // flip.onclick = () => {
+    //     back.classList.remove("flip");
+    //     front.classList.add("flip");
+    //     const vid1 = document.querySelector(".break-mov");
+    //     vid1.onmouseover = () => {
+    //         vid1.play();
+    //     }
+    //     vid1.onmouseout = () => {
+    //         vid1.pause();
+    //     }
+    // }
 
-    skillsNew.onclick = () => {
-        front.classList.remove("flip");
-        back.classList.add("flip");
-    }
+    // skillsNew.onclick = () => {
+    //     front.classList.remove("flip");
+    //     back.classList.add("flip");
+    // }
 });
